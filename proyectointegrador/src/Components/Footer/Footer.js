aeef03f2562535d75efe8d136a13e9d9da6a371d
Ramiro2805 <rlohrmann@udesa.edu.ar>
2024-03-21 11:14:31 -0300

    Empezar el proyecto react, y un header y footer basico

---
+++ proyectointegrador/src/Components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Footer () {
+    <div>
+        <h2>Ramiro Lohrmann</h2>
+        <h2>Bautista Porcel</h2>
+    </div>
+}
---
+++ proyectointegrador/src/Components/header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    <div>\
+        <ul>
+            <li><a>Home</a></li>
+            <li><a>Favoritos</a></li>
+            <li><a>Peliculas mas populares</a></li>
+        </ul>
+    </div>
+}
